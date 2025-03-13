71b47f1a226a372dd9851f7bdd80a5c8011d455b
khaser <a-horohorin@mail.ru>
2025-03-13 21:02:31 +0300

    split project to gradle submodules & add native image build task

--- flake.nix
+++ flake.nix
@@ -4,18 +4,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs-2311.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-2311, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
     flake-utils.lib.eachDefaultSystem ( system:
     let
       pkgs = import nixpkgs { inherit system; };
-      pkgs-2311 = import nixpkgs-2311 { inherit system; };
+      pkgs-unstable = import nixpkgs-unstable { inherit system; };
       buildDunePackage = pkgs.ocamlPackages.callPackage ./dune.nix {};
       logger-p5 = pkgs.callPackage ./logger.nix {};
       GT = pkgs.callPackage ./GT.nix { inherit buildDunePackage logger-p5; };
       ostap = pkgs.callPackage ./ostap.nix { inherit buildDunePackage GT; };
+      graalvm = pkgs-unstable.graalvm-ce;
     in {
       devShell = pkgs.mkShell {
         name = "plvm-lama";
@@ -23,7 +24,11 @@
         nativeBuildInputs = with pkgs; [
           gdb gcc_multi
           clang-tools_16
-          gradle
+          graalvm
+          (pkgs-unstable.gradle.override {
+            java = graalvm;
+          })
+          jetbrains.idea-community
         ] ++ (with pkgs.ocamlPackages; [
           findlib
           ocaml
@@ -31,9 +36,6 @@
           camlp5
           ostap
           GT
-        ]) ++ (with pkgs-2311; [
-          graalvm-ce
-          maven
         ]);
       };
     });
