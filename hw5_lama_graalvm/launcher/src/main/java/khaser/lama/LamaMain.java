e35724562939c02a41d5a9c9116deb8f9349e08c
khaser <a-horohorin@mail.ru>
2025-03-21 17:41:01 +0300

    add options parsing to launcher

--- hw5_lama_graalvm/launcher/src/main/java/khaser/lama/LamaMain.java
+++ hw5_lama_graalvm/launcher/src/main/java/khaser/lama/LamaMain.java
@@ -3,11 +3,9 @@ package khaser.lama;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.PolyglotException;
@@ -19,19 +17,34 @@ public final class LamaMain {
     private static final String Lama = "lama";
 
     public static void main(String[] args) throws IOException {
-        assert args.length == 2 : "Lama truffle only accepts single file";
-        Source source;
+        Map<String, String> options = new HashMap<>();
 
-        source = Source.newBuilder(Lama, new File(args[0])).build();
+        String filename = null;
+        for (String arg : args) {
+            if (!parseOption(options, arg)) {
+                if (filename == null) {
+                    filename = arg;
+                } else {
+                    System.err.println("Unrecognized option: " + arg);
+                }
+            }
+        }
 
-        System.exit(executeSource(source, System.in, System.out));
+        if (filename != null) {
+            Source source = Source.newBuilder(Lama, new File(filename)).build();
+            System.exit(executeSource(source, System.in, System.out, options));
+        } else {
+            System.err.println("Filename to execute not found");
+            System.exit(1);
+        }
+
     }
 
-    private static int executeSource(Source source, InputStream in, PrintStream out) {
+    private static int executeSource(Source source, InputStream in, PrintStream out, Map<String, String> options) {
         Context context;
         PrintStream err = System.err;
         try {
-            context = Context.newBuilder(Lama).in(in).out(out).allowAllAccess(true).build();
+            context = Context.newBuilder(Lama).in(in).out(out).options(options).allowAllAccess(true).build();
         } catch (IllegalArgumentException e) {
             err.println(e.getMessage());
             return 1;
@@ -58,5 +71,30 @@ public final class LamaMain {
             context.close();
         }
     }
+    private static boolean parseOption(Map<String, String> options, String arg) {
+        if (arg.length() <= 2 || !arg.startsWith("--")) {
+            return false;
+        }
+        int eqIdx = arg.indexOf('=');
+        String key;
+        String value;
+        if (eqIdx < 0) {
+            key = arg.substring(2);
+            value = null;
+        } else {
+            key = arg.substring(2, eqIdx);
+            value = arg.substring(eqIdx + 1);
+        }
 
+        if (value == null) {
+            value = "true";
+        }
+        int index = key.indexOf('.');
+        String group = key;
+        if (index >= 0) {
+            group = group.substring(0, index);
+        }
+        options.put(key, value);
+        return true;
+    }
 }
