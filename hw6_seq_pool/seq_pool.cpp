7ab622b156d258c759bda0ad24db9570ded06b66
khaser <a-horohorin@mail.ru>
2025-03-28 03:25:15 +0300

    simplify seq_pool

--- hw6_seq_pool/seq_pool.cpp
+++ hw6_seq_pool/seq_pool.cpp
@@ -14,6 +14,8 @@
 namespace {
 struct sigaction cur_handler;
 struct sigaction prev_handler;
+std::vector<std::pair<char*, char*>> guard_zones;
+static bool isHandlerRegistred;
 
 class PoolDepletionLogger {
 
@@ -22,35 +24,26 @@ class PoolDepletionLogger {
   static PoolDepletionLogger* ptr;
   static constexpr char msg[] = "SeqPool exhausted!\n";
 
-  std::vector<std::pair<char*, char*>> guard_zones;
-
 public:
 
   PoolDepletionLogger(PoolDepletionLogger &other) = delete;
   void operator=(const PoolDepletionLogger &) = delete;
 
-  static PoolDepletionLogger *get() {
-    if (ptr) {
-      return ptr;
-    } else {
+  static void regHandler() {
+    if (!isHandlerRegistred) {
       cur_handler.sa_flags = SA_SIGINFO;
       cur_handler.sa_sigaction = handler;
       sigemptyset(&cur_handler.sa_mask);
       assert(sigaction(SIGSEGV, &cur_handler, &prev_handler) == 0);
-      return ptr = new PoolDepletionLogger;
     }
-  }
-
-  void addGuard(const std::pair<char*, char*>& guard_zone) {
-    guard_zones.push_back(guard_zone);
   }
 
   // Should be reentrant
   static void handler(int sig, siginfo_t *info, void *ucontext) {
     char* mem_hit = (char*) info->si_addr;
 
-    for (const auto& zone : PoolDepletionLogger::get()->guard_zones) {
-      if (zone.first <= mem_hit && mem_hit <= zone.second) {
+    for (const auto& zone : guard_zones) {
+      if (zone.first <= mem_hit && mem_hit < zone.second) {
         write(STDERR_FILENO, msg, strlen(msg));
         break;
       }
@@ -83,7 +76,8 @@ SeqPool::SeqPool(size_t pool_size, size_t max_alloc_sz) {
   alloc_start = guard_end + pgs_to_alloc * pg_size;
   free_start = alloc_start;
 
-  PoolDepletionLogger::get()->addGuard({guard_end, alloc_end});
+  guard_zones.push_back({guard_end, alloc_end});
+  PoolDepletionLogger::regHandler();
 }
 
 void* SeqPool::alloc(size_t bytes) {
